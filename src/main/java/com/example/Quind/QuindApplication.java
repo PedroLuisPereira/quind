4b59efb978f5526b9bbe919643578ea6da0456bd
CEIBA\pedro.pereira <ppereira@sura.com.co>
2024-05-07 20:13:11 -0500

    primer commit

---
+++ src/main/java/com/example/Quind/QuindApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuindApplication.class, args);
+	}
+
+}
